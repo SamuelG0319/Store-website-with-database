e019fc1e842c3f721bc2c735f36c8606b59c4f26
samuel <samuel.lasso@utp.ac.pa>
2023-10-22 22:22:12 -0500

    Creation and implementation of a responsive table
    A table was created that is fully responsive to the amount of data that is made in the SELECT.

--- actividad_5/mostrar_tablas.php
+++ actividad_5/mostrar_tablas.php
@@ -22,14 +22,26 @@ if ($result) {
 <html>
 
 <head>
+
+    <meta charset="UTF-8">
+
     <title>La Ruta 66 - Tablas</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 
     <link rel="stylesheet" href="assets/css/index_style.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&amp;display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/fonts/icomoon/style.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="assets/css/table_style.css">
+
 </head>
 
-<body class="img js-fullheight" style="background-image: url(assets/images/bg.jpg);">
+<body class="img js-fullheight">
     <div class="container">
         <div class="main-body">
 
@@ -43,8 +55,8 @@ if ($result) {
             </nav>
             <!-- /Breadcrumb -->
 
-            <div class="row gutters-sm">
-                <form method="post" action="mostrar_tablas.php">
+            <div class="container" style="text-align:center;">
+                <form method="post" action="mostrar_tablas.php" style="display: inline-block;">
                     <label for="table">Selecciona una tabla:</label>
                     <select name="table" id="table">
                         <?php
@@ -55,7 +67,10 @@ if ($result) {
                     </select>
                     <input type="submit" value="Mostrar datos">
                 </form>
+                <hr>
+            </div>
 
+            <div class="container" style="text-align: center;">
                 <?php
                 if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     $selectedTable = $_POST["table"];
@@ -64,30 +79,41 @@ if ($result) {
                     $result = $dbconn->query($sql);
 
                     if ($result) {
-
                         echo "<h2>Datos de la tabla {$selectedTable}</h2>";
-                        echo "<table border='1'>";
+                        echo "<hr>";
+
+                ?>
+            </div>
+
+            <div class="table-responsive">
+
+                <?php
+                        echo "<table class='table custom-table'>";
+                        echo "<thead>";
                         echo "<tr>";
                         for ($i = 0; $i < $result->columnCount(); $i++) {
                             $columnMeta = $result->getColumnMeta($i);
-                            echo "<th>{$columnMeta['name']}</th>";
+                            echo "<th scope='col'>{$columnMeta['name']}</th>";
                         }
                         echo "</tr>";
+                        echo "</thead>";
+                ?>
 
+                <tbody>
+            <?php
                         while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                            echo "<tr>";
+                            echo "<tr class='row100 body'>";
                             foreach ($row as $valor) {
                                 echo "<td>{$valor}</td>";
                             }
                             echo "</tr>";
                         }
-                        echo "</table>";
                     } else {
-                        echo "Error al consultar la tabla.";
+                        echo "Error al consultar la tabla";
                     }
                 }
-                ?>
-
+            ?>
+                </tbody>
             </div>
 
         </div>
@@ -97,6 +123,13 @@ if ($result) {
     <script src="assets/js/popper.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
     <script src="assets/js/main.js"></script>
+
+    <script src="assets/js/table_main.js"></script>
+    <script defer
+        src="https://static.cloudflareinsights.com/beacon.min.js/v84a3a4012de94ce1a686ba8c167c359c1696973893317"
+        integrity="sha512-euoFGowhlaLqXsPWQ48qSkBSCFs3DPRyiwVu3FjR96cMPx+Fr+gpWRhIafcHwqwCqWS42RZhIudOvEI+Ckf6MA=="
+        data-cf-beacon='{"rayId":"81a6a4927f27daa3","b":1,"version":"2023.10.0","token":"cd0b4b3a733644fc843ef0b185f98241"}'
+        crossorigin="anonymous"></script>
 
 </body>
 
